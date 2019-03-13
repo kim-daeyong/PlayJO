0f8307b6d46c74455afe288fb155d234311a8c92
kim-daeyong <kdy2353@gmail.com>
2019-03-13 13:14:07 +0900

    프로젝트 생성

---
+++ src/main/java/com/playjo/project/playjo/PlayjoApplication.java
@@ -0,0 +1,13 @@
+package com.playjo.project.playjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayjoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayjoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playjo/project/playjo/PlayjoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.playjo.project.playjo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlayjoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
