bca4f37dace321ef577a194eb7af06c77c2a3501
kim-daeyong <kdy2353@gmail.com>
2019-03-19 18:30:57 +0900

    패키지 생성

---
+++ src/main/java/com/playjo/project/playjo/domain/user.java
@@ -0,0 +1,15 @@
+package com.playjo.project.playjo.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+@Setter
+@Getter
+public class user {
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,92 @@
+insert  into user (user_no,role,email,name,nickname,password,birth,regdate) values('1', '1', 'kdy2353@naver.com', 'kim','daeyong','123456','19900108',now());
+
+insert  into user (user_no,role,email,name,nickname,password,birth,regdate) values('2', '0', 'dabinchoi@naver.com', 'choi','dabin','123456','1011',now());
+
+insert  into user (user_no,role,email,name,nickname,password,birth,regdate) values('3', '0', 'kdy2353@naver.com', 'choi','wono','123456','1010',now());
+
+insert  into user (user_no,role,email,name,nickname,password,birth,regdate) values('4', '0', 'cn0@naver.com', 'seo','chungwon','123456','01010',now());
+
+insert  into user (user_no,role,email,name,nickname,password,birth,regdate) values('5', '0', 'andy@naver.com', 'min','andy','123456','0801',now());
+
+
+insert into category value('1');
+insert into category value('2');
+insert into category value('3');
+
+
+insert into list_place values('1', 'a', '1', '10000', 'seoul', 'ppap', now(),'11',  '5', '1');
+
+insert into list_place values('2', 'b', '20', '20000', 'daegu', 'ppap2', now(),'22', '4', '1');
+
+insert into list_place values('3', 'c', '30', '30000', 'daejoen', 'ppap3', now(),'33', '3', '1');
+
+insert into list_place values('4', 'd', '40', '40000', 'busan', 'ppap4', now(),'44', '2', '1');
+
+insert into list_place values('5', 'e', '50', '50000', 'mockpo', 'ppap5', now(),'55', '1', '1');
+
+
+insert into list_place values('6', '2aasd', '12', '100002', 'seoul2', '2ppap', now(),'112',  '2', '2');
+
+insert into list_place values('7', '2basd', '202', '200002', 'daegu2', '2ppap2', now(),'222', '3', '2');
+
+insert into list_place values('8', '2casd', '302', '300002', 'daejoen2', '2ppap3', now(),'332', '4', '2');
+
+insert into list_place values('9', '2dasd', '402', '400002', 'busan2', '2ppap4', now(),'442', '5', '2');
+
+insert into list_place values('10', '2easd', '502', '500002', 'mockpo2', '2ppap5', now(),'552', '1', '2');
+
+
+insert into list_place values('11', '3azxc', '11', '100003', 'seoul3', '3ppap', now(),'113',  '3', '3');
+
+insert into list_place values('12', '3bzxc', '201', '200003', 'daegu3', '3ppap2', now(),'223', '4', '3');
+
+insert into list_place values('13', '3czxc', '301', '300003', 'daejoen3', '3ppap3', now(),'333', '5', '3');
+
+insert into list_place values('14', '3dzxc', '401', '400003', 'busan3', '3ppap4', now(),'443', '1', '3');
+
+insert into list_place values('15', '3ezxc', '501', '500003', 'mockpo3', '3ppap5', now(),'553', '2', '3');
+
+
+
+INSERT INTO NOTICE VALUES('1','공지사항1번입니다','1');
+INSERT INTO NOTICE VALUES('2','공지사항2번입니다','1');
+INSERT INTO NOTICE VALUES('3','공지사항3번입니다','1');
+INSERT INTO NOTICE VALUES('4','공지사항4번입니다','1');
+INSERT INTO NOTICE VALUES('5','공지사항5번입니다','1');
+
+
+insert into review values ('1', 'daeyong', '5','wow','awesome',now(),'1','1');
+insert into review values ('2', 'daeyong', '4','wow','awesome',now(),'1','2');
+insert into review values ('3', 'daeyong', '3','wow','awesome',now(),'1','3');
+insert into review values ('4', 'daeyong', '2','wow','awesome',now(),'1','4');
+insert into review values ('5', 'daeyong', '1','wow','awesome',now(),'1','5');
+
+insert into review values ('6', 'daeyong', '5','wow2','awesome2',now(),'1','6');
+insert into review values ('7', 'daeyong', '4','wow2','awesome2',now(),'1','7');
+insert into review values ('8', 'daeyong', '3','wow2','awesome2',now(),'1','8');
+insert into review values ('9', 'daeyong', '2','wow2','awesome2',now(),'1','9');
+insert into review values ('10', 'daeyong', '1','wow2','awesome2',now(),'1','10');
+
+insert into review values ('11', 'daeyong', '5','wow3','awesome3',now(),'1','11');
+insert into review values ('12', 'daeyong', '4','wow3','awesome3',now(),'1','12');
+insert into review values ('13', 'daeyong', '3','wow3','awesome3',now(),'1','13');
+insert into review values ('14', 'daeyong', '2','wow3','awesome3',now(),'1','14');
+insert into review values ('15', 'daeyong', '1','wow3','awesome3',now(),'1','15');
+
+insert into reply values('1','wowooow','daeyong','1');
+insert into reply values('2','wowooow2','daeyong','1');
+insert into reply values('3','wowooow3','daeyong','1');
+insert into reply values('4','wowooow4','daeyong','1');
+insert into reply values('5','wowooow5','daeyong','1');
+insert into reply values('6','wowooow6','daeyong','1');
+insert into reply values('7','wowooow7','daeyong','1');
+
+
+
+insert into wishlist values('1', '1', 'a', 'seoul','1');
+insert into wishlist values('2', '2', 'a', 'seoul2','1');
+insert into wishlist values('3', '3', 'a', 'seoul3','1');
+insert into wishlist values('4', '2', 'a', 'daegu2','1');
+insert into wishlist values('5', '3', 'a', 'daegu3','1');
+
+
