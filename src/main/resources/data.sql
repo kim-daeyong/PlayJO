a0f8666e5340764c4775188065a8e3ae4e8595cc
kim-daeyong <kdy2353@gmail.com>
2019-03-20 16:03:13 +0900

    sampleData 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,42 +9,42 @@ insert  into user (user_no,role,email,name,nickname,password,birth,regdate) valu
 insert  into user (user_no,role,email,name,nickname,password,birth,regdate) values('5', '0', 'andy@naver.com', 'min','andy','123456','0801',now());
 
 
-insert into category value('1');
-insert into category value('2');
-insert into category value('3');
+insert into category value('1', 'hotel');
+insert into category value('2', 'eatery');
+insert into category value('3', 'sight');
 
 
-insert into list_place values('1', 'a', '1', '10000', 'seoul', 'ppap', now(),'11',  '5', '1');
+insert into place values('1', 'a', '1', '10000', 'seoul', 'ppap', now(),'11',  '5', '1');
 
-insert into list_place values('2', 'b', '20', '20000', 'daegu', 'ppap2', now(),'22', '4', '1');
+insert into place values('2', 'b', '20', '20000', 'daegu', 'ppap2', now(),'22', '4', '1');
 
-insert into list_place values('3', 'c', '30', '30000', 'daejoen', 'ppap3', now(),'33', '3', '1');
+insert into place values('3', 'c', '30', '30000', 'daejoen', 'ppap3', now(),'33', '3', '1');
 
-insert into list_place values('4', 'd', '40', '40000', 'busan', 'ppap4', now(),'44', '2', '1');
+insert into place values('4', 'd', '40', '40000', 'busan', 'ppap4', now(),'44', '2', '1');
 
-insert into list_place values('5', 'e', '50', '50000', 'mockpo', 'ppap5', now(),'55', '1', '1');
-
-
-insert into list_place values('6', '2aasd', '12', '100002', 'seoul2', '2ppap', now(),'112',  '2', '2');
-
-insert into list_place values('7', '2basd', '202', '200002', 'daegu2', '2ppap2', now(),'222', '3', '2');
-
-insert into list_place values('8', '2casd', '302', '300002', 'daejoen2', '2ppap3', now(),'332', '4', '2');
-
-insert into list_place values('9', '2dasd', '402', '400002', 'busan2', '2ppap4', now(),'442', '5', '2');
-
-insert into list_place values('10', '2easd', '502', '500002', 'mockpo2', '2ppap5', now(),'552', '1', '2');
+insert into place values('5', 'e', '50', '50000', 'mockpo', 'ppap5', now(),'55', '1', '1');
 
 
-insert into list_place values('11', '3azxc', '11', '100003', 'seoul3', '3ppap', now(),'113',  '3', '3');
+insert into place values('6', '2aasd', '12', '100002', 'seoul2', '2ppap', now(),'112',  '2', '2');
 
-insert into list_place values('12', '3bzxc', '201', '200003', 'daegu3', '3ppap2', now(),'223', '4', '3');
+insert into place values('7', '2basd', '202', '200002', 'daegu2', '2ppap2', now(),'222', '3', '2');
 
-insert into list_place values('13', '3czxc', '301', '300003', 'daejoen3', '3ppap3', now(),'333', '5', '3');
+insert into place values('8', '2casd', '302', '300002', 'daejoen2', '2ppap3', now(),'332', '4', '2');
 
-insert into list_place values('14', '3dzxc', '401', '400003', 'busan3', '3ppap4', now(),'443', '1', '3');
+insert into place values('9', '2dasd', '402', '400002', 'busan2', '2ppap4', now(),'442', '5', '2');
 
-insert into list_place values('15', '3ezxc', '501', '500003', 'mockpo3', '3ppap5', now(),'553', '2', '3');
+insert into place values('10', '2easd', '502', '500002', 'mockpo2', '2ppap5', now(),'552', '1', '2');
+
+
+insert into place values('11', '3azxc', '11', '100003', 'seoul3', '3ppap', now(),'113',  '3', '3');
+
+insert into place values('12', '3bzxc', '201', '200003', 'daegu3', '3ppap2', now(),'223', '4', '3');
+
+insert into place values('13', '3czxc', '301', '300003', 'daejoen3', '3ppap3', now(),'333', '5', '3');
+
+insert into place values('14', '3dzxc', '401', '400003', 'busan3', '3ppap4', now(),'443', '1', '3');
+
+insert into place values('15', '3ezxc', '501', '500003', 'mockpo3', '3ppap5', now(),'553', '2', '3');
 
 
 
@@ -73,20 +73,20 @@ insert into review values ('13', 'daeyong', '3','wow3','awesome3',now(),'1','13'
 insert into review values ('14', 'daeyong', '2','wow3','awesome3',now(),'1','14');
 insert into review values ('15', 'daeyong', '1','wow3','awesome3',now(),'1','15');
 
-insert into reply values('1','wowooow','daeyong','1');
-insert into reply values('2','wowooow2','daeyong','1');
-insert into reply values('3','wowooow3','daeyong','1');
-insert into reply values('4','wowooow4','daeyong','1');
-insert into reply values('5','wowooow5','daeyong','1');
-insert into reply values('6','wowooow6','daeyong','1');
-insert into reply values('7','wowooow7','daeyong','1');
+insert into reply values('1','wowooow','daeyong','1',now());
+insert into reply values('2','wowooow2','daeyong','1',now());
+insert into reply values('3','wowooow3','daeyong','1',now());
+insert into reply values('4','wowooow4','daeyong','1',now());
+insert into reply values('5','wowooow5','daeyong','1',now());
+insert into reply values('6','wowooow6','daeyong','1',now());
+insert into reply values('7','wowooow7','daeyong','1',now());
 
 
 
-insert into wishlist values('1', '1', 'a', 'seoul','1');
-insert into wishlist values('2', '2', 'a', 'seoul2','1');
-insert into wishlist values('3', '3', 'a', 'seoul3','1');
-insert into wishlist values('4', '2', 'a', 'daegu2','1');
-insert into wishlist values('5', '3', 'a', 'daegu3','1');
+insert into wish values('1', '1', 'a', 'seoul','1');
+insert into wish values('2', '2', 'a', 'seoul2','1');
+insert into wish values('3', '3', 'a', 'seoul3','1');
+insert into wish values('4', '2', 'a', 'daegu2','1');
+insert into wish values('5', '3', 'a', 'daegu3','1');
 
 
