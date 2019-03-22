32f94d17c35df6e630510326baef0d3f1e574fa7
test <dabinnn1011@naver.com>
2019-03-22 18:32:23 +0900

    수정

--- src/main/java/com/playjo/project/playjo/domain/Place.java
+++ src/main/java/com/playjo/project/playjo/domain/Place.java
@@ -38,10 +38,7 @@ public class Place {
     @ManyToOne
     @JoinColumn(name = "cotegory_no")
     private List<Category> category;
-
-
-
-
+    
     @OneToMany(mappedBy = "review_no")
     private List<Review> reviews;
 
