81d80b4b8450d1cd9c5e872b7d12a0164415cb41
kim-daeyong <kdy2353@gmail.com>
2019-03-27 17:17:55 +0900

    #1 쿼리 작성중

--- src/test/java/com/playjo/project/playjo/repotest/PlaceRepoTest.java
+++ src/test/java/com/playjo/project/playjo/repotest/PlaceRepoTest.java
@@ -8,6 +8,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -25,12 +28,28 @@ public class PlaceRepoTest {
 
     }
 
-    @Test
-    public void findAll(){
-
+//    @Test
+//    public void findAll() {
+//
 //        List<Place> all = placeRepository.findAll();
-//        for(Place list : all){
+//        for (Place list : all) {
 //            System.out.println(list);
+//        }
+//    }
+
+
+    @Test
+    public void findrating() throws Exception{
+        Pageable page = PageRequest.of(1, 3);
+        Page<Place> all = placeRepository.getPlacesByCateNo(1,page);
+        System.out.println("place cate 1");
+        System.out.println("전체 건수 : " + all.getTotalElements());
+        for(Place place : all){
+            System.out.println("Name,위치 : " + place.getName()+ " " + place.getLocation());
+            System.out.println(" readCount : " + place.getRatingAvg());
+            System.out.println(" view : " + place.getReadCount());
         }
+    }
+
 
 }
