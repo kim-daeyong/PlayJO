be0563d4e4085dd826e1b056251dc155c11ed0e9
test <dabinnn1011@naver.com>
2019-04-01 15:46:07 +0900

    ReplyRepository

--- src/main/java/com/playjo/project/playjo/repository/ReplyRepository.java
+++ src/main/java/com/playjo/project/playjo/repository/ReplyRepository.java
@@ -1,6 +1,8 @@
 package com.playjo.project.playjo.repository;
 
 import com.playjo.project.playjo.domain.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +11,7 @@ import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
-//    @Query("SELECT r From Reply r WHERE r.review.review_no = :review_no")
-//    public List<Reply> getReply(@Param("review_no") Long review_no);
+    @Query("SELECT rr FROM Reply rr where rr.review.id = :id order by rr.id desc")
+    Page<Reply> getReplyByReplyNo(@Param("id")Long id, Pageable pageable);
 }
 
