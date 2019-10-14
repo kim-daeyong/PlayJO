ca23db5af8f93f76008150d4bec99667c1e38aec
kim-daeyong <kdy2353@gmail.com>
2019-10-14 23:55:02 +0900

    restcontroller

--- src/main/java/com/playjo/project/playjo/controller/api/RoleController.java
+++ src/main/java/com/playjo/project/playjo/controller/api/RoleController.java
@@ -1,15 +1,16 @@
-package com.playjo.project.playjo.controller;
+package com.playjo.project.playjo.controller.api;
 
 
 import com.playjo.project.playjo.domain.Role;
-import com.playjo.project.playjo.service.impl.RoleService;
+import com.playjo.project.playjo.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RoleController {
 
     @Autowired
--- src/main/java/com/playjo/project/playjo/controller/api/WishController.java
+++ src/main/java/com/playjo/project/playjo/controller/api/WishController.java
@@ -1,12 +1,13 @@
-package com.playjo.project.playjo.controller;
+package com.playjo.project.playjo.controller.api;
 
 
 import com.playjo.project.playjo.service.impl.WishService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/wishs")
 @RequiredArgsConstructor
 public class WishController {
